bccee231248e342cce81c6507458333c7d3defde
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-05-31 02:15:55 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,77 @@
+from itertools import product
+from numpy import loadtxt
+fin_list = []
+def combinations(numbers_to_use,target):
+    output = []
+    lst = []
+    for n in range(1, target + 1):
+        numbers = product(numbers_to_use, repeat=n)
+        lst.append(list(numbers))
+    for i in lst:
+        for j in i:
+            if sum(j) == target:
+                output.append(j)
+    return output
+
+
+elements=['H', 'B', 'C', 'N', 'O', 'F', 'P', 'S', 'K', 'V', 'Y', 'I', 'W', 'U', 'HE', 'LI', 'BE', 'NE', 'NA', 'MG', 'AL', 'SI', 'CL', 'AR', 'CA', 'SC', 'TI', 'CR', 'MN', 'FE', 'CO', 'NI', 'CU', 'ZN', 'GA', 'GE', 'AS', 'SE', 'BR', 'KR', 'RB', 'SR', 'ZR', 'NB', 'MO', 'TC', 'RU', 'RH', 'PD', 'AG', 'CD', 'IN', 'SN', 'SB', 'TE', 'XE', 'CS', 'BA', 'LA', 'CE', 'PR', 'ND', 'PM', 'SM', 'EU', 'GD', 'TB', 'DY', 'HO', 'ER', 'TM', 'YB', 'LU', 'HF', 'TA', 'RE', 'OS', 'IR', 'PT', 'AU', 'HG', 'TL', 'PB', 'BI', 'PO', 'AT', 'RN', 'FR', 'RA', 'AC', 'TH', 'PA', 'NP', 'PU', 'AM', 'CM', 'BK', 'CF', 'ES', 'FM', 'MD', 'NO', 'LR', 'RF', 'DB', 'SG', 'BH', 'HS', 'MT', 'DS', 'RG', 'CN', 'NH', 'FL', 'MC', 'LV', 'TS', 'OG']
+
+
+def chem_name(string):
+    copy= string
+    string = string.upper()
+    comb = combinations([1,2], len(string))
+    output = list()
+    out = list()
+    for i in comb:
+        for j in comb:
+            string=copy
+            out.append(" ")
+            for k in j:
+                out.append(string[:k])
+                string = string[k:]
+                if string[:k] == "":
+                    break
+    lst=[]
+    out=out[1:]
+    copy = []
+    for i in out:
+        if i ==" ":
+            copy.append(lst)
+            lst = []
+        else:
+            lst.append(i)
+    for i in copy:
+        if set(i).issubset(elements):
+            output.append(i)
+    if len(output)>0:
+        output = output[0]
+        for n, i in enumerate(output):
+            if len(i)>1:
+                i[1].lower()
+                output[n] = i[0]+i[1].lower()
+        return output
+    else:
+        return False
+
+f = open('words.txt', 'r+')
+lines = [line for line in f.readlines()]
+f.close()
+word_list =list(map(lambda s: s.strip(), lines))
+print(len(word_list))
+
+
+for i in word_list:
+    i = i.upper()
+    k = chem_name(i)
+    if k!=False:
+        print(i)
+        fin_list.append(k)
+
+print(fin_list)
+print()
+with open("final.txt", "w") as txt_file:
+    for line in fin_list:
+        txt_file.write(",".join(line) + "\n")
+
+
